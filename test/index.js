6740a080c977214919d23252620829af8386c431
Julie Nisbet <jmnisbet@gmail.com>
2022-11-17 09:01:17 -0800

    get supabase working

--- test/index.js
+++ test/index.js
@@ -2,11 +2,13 @@
 
 // include jsdom for DOM use in tests on travis
 const jsdom = require('jsdom');
+const supabase = require('@supabase/supabase-js');
 const { JSDOM } = jsdom;
 const { window } = new JSDOM(``, {
-    url: 'http://localhost:5500'
+    url: 'http://localhost:5500',
 });
 global.window = window;
+global.supabase = supabase;
 global.document = window.document;
 global.FormData = window.FormData;
 global.localStorage = window.localStorage;
